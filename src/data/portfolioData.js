a706e2f49892ba189d831531f22aaddeeee3c53f
Mounif Abdoulatif <mounif2012@gmail.com>
2021-10-07 19:43:23 +0200

    màj projet virtuality

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,12 +1,12 @@
 export const portfolioData = [
 	{
 		id: 1,
-		name: "ipsum.com",
-		languages: ["javascript", "css"],
-		languagesIcons: ["fab fa-js", "fab fa-css3-alt"],
-		source: "https://github.com/mounifABDlatif",
-		info: "Ce projet sera bientôt mis dans le portefolio.",
-		picture: "./media/focus-animation.gif",
+		name: "Virtuality",
+		languages: ["javascript", "css", "php"],
+		languagesIcons: ["fab fa-js", "fab fa-sass", "fab fa-symfony"],
+		source: "https://github.com/lotusbleudev/virtuality/tree/dev",
+		info: "Ce projet est un travail collaboratif réalisé par moi-même et 2 de mes camarades, durant un bootcamp que j'ai effectué entre le mois de juin 2021 et octobre 2021. Il s'agit d'un site vitrine pour une salle de jeu VR (fictive) appelée VIRTUALITY. Le site permet aux utilisateurs de se créer un compte, de consulter nos jeux disponibles, nos tarifs et les actus. Le site permet aussi une réservation en ligne des salles ainsi que l’inscription aux tournois/événements. Les utilisateurs pourront aussi se désinscrire à tout moment. Chaque utilisateur a la possibilité de modifier ces données personnelles en accédant à son compte dans lequel il pourra aussi consulter ses réservations. Pour créer ce site nous avons utilisé Symfony en Back-end avec php et twig. En Front, nous avons utilisés du javascript natif et du Scss. Pour assurer l'interaction avec la bdd, nous avons intégré un CRUD avec une accessibilité complète pour les admins mais réduites pour les clients. Il y a aussi un système de mailing avec Swiftmailer pour la confirmation d'inscription et de réservation; On utilise aussi le Swiftmailer ans un formulaire de contacte intégrer au site",
+		picture: "./media/virtuality.jpg",
 	},
 	{
 		id: 2,
