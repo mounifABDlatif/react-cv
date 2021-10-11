470358dafa2cc793b8483665eee7af3fbca84d62
Mounif Abdoulatif <mounif2012@gmail.com>
2021-10-11 18:29:03 +0200

    màj portfolio/virtuality

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -6,7 +6,7 @@ export const portfolioData = [
 		languagesIcons: ["fab fa-js", "fab fa-sass", "fab fa-symfony"],
 		source: "https://github.com/mounifABDlatif/virtuality/tree/dev",
 		info: ["Virtuality est un site appartenant à une entreprise(fictive), ayant le même nom, qui propose des salles d’arcades VR (Réalité Virtuelle). Le site permet aux utilisateurs de se créer un compte, de consulter nos jeux disponibles, nos tarifs et les actus, ainsi que de se réserver des créneaux. ", 
-		"Les stacks techniques utilisées sont Javascript et Sass en Front et Symfony en BackEnd. ",
+		" Les stacks techniques utilisées sont Javascript et Sass en Front et Symfony en BackEnd. ",
 		"Mon rôle était celui d'un dev full-stack je me suis chargé de la page de 'Réservation' qui consiste d'un système de CRUD permettant aux utilisateurs de se réserver un créneau après être connecté. J'ai mis en place une restriction d'accès qui permet uniquement aux admins de gérer la suppression et la modification des créneaux en plus de la création. Un mail de confirmation est envoyé aux clients à chaque réservation grâce à Swiftmailer. J'utilise aussi ce dernier dans un formulaire de contact réservé aux utilisateurs du site. ",
 		"Ce projet est un travail collaboratif réalisé par moi-même et 2 de mes camarades, durant un bootcamp que j'ai effectué entre le mois de juin 2021 et octobre 2021."],
 		picture: "./media/virtuality.jpg",
