7dc342639110e8a9f326ddc4eba67509636632e2
Mounif Abdoulatif <mounif2012@gmail.com>
2021-12-01 16:31:09 +0100

    màj

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -6,18 +6,22 @@ const Experience = () => {
             <h3>Expériences</h3>
 
             <div className="exp-1">
+                <h4>Développeur Fullstack</h4>
+                <h6>Virtuality</h6>
+                <h5>Août 2021 - Octobre 2021</h5>
+                <p>Les stacks techniques utilisées : Javascript et Sass en Front - Symfony en BackEnd - Git / Github pour le travail en équipe<br />
+
+                Mes missions en tant que dev full-stack :
+                Chargé de la page de "Réservation" (front/back). - Mise en place d'un CRUD : réservation d'un créneau après la connexion + contrôle/sécurité. - Restriction d'accès au Backoffice. - Envoi de mail de confirmation avec Swiftmailer. - Mise en place d'un formulaire de contact pour les clients. - Template Twig,<br /><br /></p>
+            </div>
+
+            <div className="exp-2">
                 <h4>Moniteur étudiant à ETN@ - Le mans Université </h4>
                 <h6>DSI - Le Mans Université</h6>
                 <h5>Septembre 2019 - Août 2020 (10 mois)</h5>
                 <p>Soutien informatique et aide à l'utilisation des nouvelles technologies</p>
             </div>
-            <div className="exp-2">
-                <h4>Interprète indépendant (stage+bénévolat)</h4>
-                <h6>Asso Tadamoon</h6>
-                <h5>Janvier 2019 - Septembre 2019</h5>
-                <p>Traduire oralement depuis le Français et vers le Français.
-                Accompagnement des réfugiés dans les procédures administratives.</p>
-            </div>
+            
         </div>
     );
 };
